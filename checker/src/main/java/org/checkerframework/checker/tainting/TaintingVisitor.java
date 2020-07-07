f4125738d11e105d357ed2d8b52fa530ad2c2aa3
aditya3434 <aditya390402@gmail.com>
2020-07-08 00:14:03 +0530

    Implementing indirect information flow checks

--- checker/src/main/java/org/checkerframework/checker/tainting/TaintingVisitor.java
+++ checker/src/main/java/org/checkerframework/checker/tainting/TaintingVisitor.java
@@ -1,10 +1,15 @@
 package org.checkerframework.checker.tainting;
 
+import com.sun.source.tree.*;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.type.TypeKind;
+import org.checkerframework.checker.tainting.qual.Untainted;
 import org.checkerframework.common.basetype.BaseAnnotatedTypeFactory;
 import org.checkerframework.common.basetype.BaseTypeChecker;
 import org.checkerframework.common.basetype.BaseTypeVisitor;
+import org.checkerframework.framework.type.AnnotatedTypeMirror;
 import org.checkerframework.framework.type.AnnotatedTypeMirror.AnnotatedExecutableType;
+import org.checkerframework.javacutil.TreeUtils;
 
 /** Visitor for the {@link TaintingChecker}. */
 public class TaintingVisitor extends BaseTypeVisitor<BaseAnnotatedTypeFactory> {
@@ -25,4 +30,62 @@ public class TaintingVisitor extends BaseTypeVisitor<BaseAnnotatedTypeFactory> {
     @Override
     protected void checkConstructorResult(
             AnnotatedExecutableType constructorType, ExecutableElement constructorElement) {}
+
+    /**
+     * Checks whether indirect information flow cannot take place in given condition check.
+     *
+     * @param tree of condition statement that needs to be checked
+     */
+    private void checkCondition(ExpressionTree tree) {
+        if (tree.getKind().asInterface().equals(BinaryTree.class)) {
+            AnnotatedTypeMirror lhs =
+                    atypeFactory.getAnnotatedType(((BinaryTree) tree).getLeftOperand());
+            AnnotatedTypeMirror rhs =
+                    atypeFactory.getAnnotatedType(((BinaryTree) tree).getRightOperand());
+            if ((lhs.hasAnnotation(Untainted.class)) ^ (rhs.hasAnnotation(Untainted.class))
+                    && (!lhs.getKind().equals(TypeKind.NULL))
+                    && (!rhs.getKind().equals(TypeKind.NULL))) {
+                checker.reportError(tree, "condition.flow.unsafe", lhs, rhs);
+            }
+        }
+    }
+
+    @Override
+    public Void visitIf(IfTree node, Void p) {
+        checkCondition(TreeUtils.withoutParens(node.getCondition()));
+        return super.visitIf(node, p);
+    }
+
+    @Override
+    public Void visitSwitch(SwitchTree node, Void p) {
+        checkCondition(TreeUtils.withoutParens(node.getExpression()));
+        return super.visitSwitch(node, p);
+    }
+
+    @Override
+    public Void visitCase(CaseTree node, Void p) {
+        if (node.getExpression() != null)
+            checkCondition(TreeUtils.withoutParens(node.getExpression()));
+        return super.visitCase(node, p);
+    }
+
+    @Override
+    public Void visitDoWhileLoop(DoWhileLoopTree node, Void p) {
+        checkCondition(TreeUtils.withoutParens(node.getCondition()));
+        return super.visitDoWhileLoop(node, p);
+    }
+
+    @Override
+    public Void visitWhileLoop(WhileLoopTree node, Void p) {
+        checkCondition(TreeUtils.withoutParens(node.getCondition()));
+        return super.visitWhileLoop(node, p);
+    }
+
+    @Override
+    public Void visitForLoop(ForLoopTree node, Void p) {
+        if (node.getCondition() != null) {
+            checkCondition(TreeUtils.withoutParens(node.getCondition()));
+        }
+        return super.visitForLoop(node, p);
+    }
 }
